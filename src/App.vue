5b6715515b41a16745ccdec8cec0ef0fb41cb4cb
Sergey Ipatov <sergey.ipatov@blip.pt>
2018-02-02 21:50:53 +0000

    Simple binded model test.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Search</router-link>
     </div>
+    <button @click="askPermission">Login with Spotify</button>
     <router-view/>
   </div>
 </template>
 
+<script lang="ts">
+  import { Component, Model, Vue } from 'vue-property-decorator';
+  // import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+  @Component({
+    components: {},
+  })
+  export default class App extends Vue {
+    private askPermission() {
+      // console.log(this.$store.state.test);
+    }
+  }
+</script>
+
 <style>
+
+* {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -26,7 +45,7 @@
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
+#nav a.router-link-active {
   color: #42b983;
 }
 </style>
---
+++ src/views/Search.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="search">
+    <input class="searcher" type="text" v-model="searchTerm" placeholder="Search for something music related..."/>
+    <div class="progress" v-if="!!searchTerm">
+      <span class="progress__message">Searching {{searchLoading}}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Model, Vue } from 'vue-property-decorator';
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {},
+})
+export default class Search extends Vue {
+  private searchTerm: string = '';
+
+  get searchLoading() {
+    return this.searchTerm + '...';
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.searcher {
+  padding: 5px;
+  outline: 0;
+  border: 0;
+  border-radius: 3px;
+  width: 100%;
+  height: 30px;
+  box-shadow: inset 0 0 3px black;
+  background-color: rgb(252, 252, 252);
+}
+
+</style>
