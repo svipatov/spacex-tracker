64b5b2b1b9fe9fe91b385e6ff3028a6d71d99aec
Sergey Ipatov <sergey.ipatov@blip.pt>
2018-02-04 22:17:45 +0000

    Launch page implemented

--- src/store/index.js
+++ src/store/index.js
@@ -29,7 +29,11 @@ const getters = {
     return state.launches
   },
   getLaunch (state) {
-    return (flight) => state.launches.find((launch) => launch.flight_number === flight)
+    return (flight) => {
+      return state.launches.find((launch) => {
+        return launch.flight_number === Number(flight)
+      })
+    }
   }
 }
 
