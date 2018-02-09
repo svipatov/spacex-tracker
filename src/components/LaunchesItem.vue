d76f978215dd25b7fa2646447bfd1c8634b3b21b
Rafael Ramalho <rafaelramalho@blip.pt>
2018-02-09 15:35:03 +0000

    Added models

--- src/components/LaunchesItem.vue
+++ src/components/LaunchesItem.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="launch">
-    <div class="launch__details">
-      <span>{{launch.flight_number}}.</span>
-      <span>{{launch.rocket.rocket_name}}</span>
-      <div class="launch-info">{{launch.details}}</div>
+    <div class="details">
+      <span class="identifier">{{ launch.id }}</span>
+      <span class="name">{{ launch.rocket.name }}</span>
+      <div class="date">{{ date }}</div>
+      <div class="info">{{ launch.details }}</div>
     </div>
-    <div class="launch__more-details">
+    <div class="more-details">
       <slot name="link"></slot>
     </div>
   </div>
@@ -13,25 +14,79 @@
 
 <script>
 export default {
-  props: ['launch']
+  props: ['launch'],
+  computed: {
+    date () {
+      const date = this.launch.date
+      const month = date.getMonth() + 1
+      const day = date.getDay()
+      const formatedMonth = month > 10 ? month : '0' + month
+      const formatedDay = day > 10 ? day : '0' + day
+
+      return `${formatedDay}-${formatedMonth}-${date.getFullYear()}`
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-	.launch {
-    display: flex;
-    flex-direction: column;
-		border: solid 1px black;
-    border-radius: 3px;
+@mixin border {
+  position: relative;
+  &::before {
+    content: "";
+    position: absolute;
+    width: calc(100% + 10px);
+    height: 1px;
 
-    &__details {
-      height: 100%;
-      padding: 5px;
-    }
-    
-    &__more-details {
-      border-top: 1px solid black;
-      padding: 5px;
-    }
-	}
+    top: 0;
+    left: -5px;
+
+    background-color: #000;
+  }
+}
+
+.launch {
+  display: flex;
+  flex-direction: column;
+  border-radius: 4px;
+  border: 1px solid black;
+}
+
+.name {
+  font-size: 18px;
+}
+
+.date {
+  padding: 4px 0 8px;
+  font-size: 14px;
+  color: grey;
+}
+
+.details {
+  position: relative;
+  height: 100%;
+  padding: 5px;
+}
+
+.identifier {
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  border-radius: 3px 3px 6px 3px;
+  padding: 3px 10px;
+
+  color: white;
+  background-color: #35495e;
+}
+
+.info {
+  @include border;
+  padding: 14px 8px;
+}
+
+.more-details {
+  @include border;
+  padding: 5px;
+}
 </style>
