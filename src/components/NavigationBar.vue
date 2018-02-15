b82e92b8fb6edf7b2cc8091f7aa1debed5f2836b
Rafael Ramalho <rafazelramalho19@gmail.com>
2018-02-15 18:27:27 +0000

    Moved date to an helper script and fixed some stylings in the links

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,45 +1,53 @@
 <template>
   <header class="header">
-		<span class="header__title">SpaceX</span>
-		<nav class="navigation">
-			<router-link class="router-link" to="/launches">Launches</router-link>
-			<router-link class="router-link" to="/rockets">Rockets</router-link>
-		</nav>
-	</header>
+    <span class="header__title">SpaceX</span>
+    <nav class="navigation">
+      <router-link class="router-link router-link--big link" to="/launches">Launches</router-link>
+      <router-link class="router-link router-link--big link" to="/rockets">Rockets</router-link>
+    </nav>
+  </header>
 </template>
 
 <style lang="scss" scoped>
-	@import '../assets/_colors.scss';
+@import "../assets/_colors.scss";
 
-	.header {
-		display: flex;
-		grid-area: header;
-		align-items: center;
-		position: sticky;
-    top: 0;
-		
-		margin: 0;
-		padding: 0 16px;
+.header {
+  display: flex;
+  grid-area: header;
+  align-items: center;
+  position: sticky;
+  top: 0;
 
-		height: 56px;
-		background-color: #35495E;
+  margin: 0;
+  padding: 0 16px;
 
-		color: #ffffff;
-		justify-content: space-between;
+  height: 56px;
+  background-color: #35495e;
 
-		&__title {
-			display: block;
-			position: relative;
-			font-size: 20px;
-			line-height: 1;
-			letter-spacing: .02em;
-			font-weight: 400;
-			box-sizing: border-box;
-		}
-	}
+  color: #ffffff;
+  justify-content: space-between;
 
-	.navigation {
-		text-transform: uppercase;
-	}
+  z-index: 2;
+
+  &__title {
+    display: block;
+    position: relative;
+    font-size: 20px;
+    line-height: 1;
+    letter-spacing: 0.02em;
+    font-weight: 400;
+    box-sizing: border-box;
+  }
+}
+
+.navigation {
+  display: flex;
+  justify-content: space-between;
+  text-transform: uppercase;
+}
+
+.link {
+  margin: 0 8px;
+}
 </style>
 
--- src/models/launch.js
+++ src/models/launch.js
@@ -15,6 +15,7 @@ const schema = Yup.object().shape({
 
 class Model {
   constructor (launch) {
+    console.log(launch)
     this.id = launch.flight_number
     this.rocket = buildRocketModel(launch.rocket)
     this.details = launch.details
