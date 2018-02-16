a6b4c0bda715353cfaec711e7c498e28f912a147
Rafael Ramalho <rafazelramalho19@gmail.com>
2018-02-16 18:03:22 +0000

    Added tests to the LaunchDetails component

---
+++ src/components/LaunchDetails/LaunchDetails.test.js
@@ -0,0 +1,39 @@
+import LaunchDetails from './LaunchDetails'
+import { shallow } from 'vue-test-utils'
+
+describe('Components - LaunchDetails', function () {
+  this.buildComponent = props => shallow(LaunchDetails, { propsData: props }).vm
+
+  describe('Computed properties', () => {
+    describe('@Success', () => {
+      it("should return false when the launch's success is null", () => {
+        const cmp = this.buildComponent({
+          launch: {
+            success: null
+          }
+        })
+        expect(cmp.success).toBe(false)
+      })
+
+      it("should return Successful when the launch's success is truthy", () => {
+        const cmp = this.buildComponent({
+          launch: {
+            success: true
+          }
+        })
+
+        expect(cmp.success).toBe('Successful')
+      })
+
+      it("should return Failed when the launch's success is falsy", () => {
+        const cmp = this.buildComponent({
+          launch: {
+            success: false
+          }
+        })
+
+        expect(cmp.success).toBe('Failed')
+      })
+    })
+  })
+})
