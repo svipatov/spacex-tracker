f443403845c2be2cc3cf7322f933d115095c0390
Rafael Ramalho <rafazelramalho19@gmail.com>
2018-02-25 12:36:45 +0000

    Finished test to the getters (launch view)

--- src/views/Launch/Launch.test.js
+++ src/views/Launch/Launch.test.js
@@ -2,27 +2,36 @@ import View from './Launch'
 import { shallow, createLocalVue } from 'vue-test-utils'
 import Vuex from 'vuex'
 
-const localVue = createLocalVue()
-localVue.use(Vuex)
-
 describe('Views - Launch', function () {
-  const store = new Vuex.Store({
-    getters: {
-      'launches/getLaunch': flight => ({
-        id: flight,
-        details: 'xpto'
-      })
-    },
-    actions: {
-      'launches/fetchLaunch': jest.fn()
+  beforeEach(() => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+
+    this.launch = {
+      id: 1
     }
+    const getters = {
+      getLaunch: flight => flight => this.launch
+    }
+    const actions = {
+      fetchLaunch: jest.fn()
+    }
+    const store = new Vuex.Store({
+      modules: {
+        launches: {
+          getters,
+          actions,
+          namespaced: true
+        }
+      }
+    })
+    this.buildComponent = props =>
+      shallow(View, {
+        propsData: props,
+        store,
+        localVue
+      }).vm
   })
-  this.buildComponent = props =>
-    shallow(View, {
-      propsData: props,
-      store,
-      localVue
-    }).vm
 
   describe('Computed properties', () => {
     describe('@launch', () => {
@@ -30,7 +39,7 @@ describe('Views - Launch', function () {
         const cmp = this.buildComponent({
           flight: 1
         })
-        expect(cmp.success).toBe(false)
+        expect(cmp.launch).toBe(this.launch)
       })
     })
   })
